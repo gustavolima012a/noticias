abb8a72e4785ae2268c83f09af2318a20e478b38
Gustavo Lima <gustavo.lima@eaportal.org>
2025-06-08 21:33:38 -0300

    criação da nav desktop e remoção da classe header-mobile e inicio da responsividade

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
-const botaoFechar = document.querySelector(".header-mobile nav button")
+const botaoAbrir = document.querySelector("header > button");
+const botaoFechar = document.querySelector("header nav button");
 
-botaoAbrir.addEventListener('click', abrirMenu)
-botaoFechar.addEventListener('click', fecharMenu)
+const body = document.querySelector("body");
+const nav = document.querySelector("header .mobile");
+
+botaoAbrir.addEventListener('click', abrirMenu);
+botaoFechar.addEventListener('click', fecharMenu);
 
 function abrirMenu() {
-    body.classList.add("menu-open")
-    nav.classList.add("abrir")
+   body.classList.add("escurecer");
+   nav.classList.add("abrir");
 }
 
 function fecharMenu() {
-    body.classList.remove("menu-open")
-    nav.classList.remove("abrir")
+   body.classList.remove("escurecer"); 
+   nav.classList.remove("abrir");
 }
